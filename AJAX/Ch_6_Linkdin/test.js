94cc4cd1f16f027e6f1f6f1d3bb89ac8d87adcd1
aniketpati <aniketp209@gmail.com>
2022-09-25 19:22:52 +0530

    Notes added in event loop

--- AJAX/Ch_6_Linkdin/test.js
+++ AJAX/Ch_6_Linkdin/test.js
@@ -1,12 +1,29 @@
 function x(){
-    let a = 7;
-    function y(){
-        console.log(a);
+    for(var i=1; i<=5; i++){
+        setTimeout(function(){
+            console.log(i, " ");
+        }, i*1000);
     }
-    return y;
 }
 
-let z = x();
-//...........
+x();
+//output of above code is 6 6 6 6 6 because of closure, it is printing
+// the reference to i.
 
-z();
+//if we use let instead, it will be 1 2 3 4 5 because let is blocked scoped 
+//else we can  add a extra function
+
+function x(){
+    for(var i=1; i<=5; i++){
+        function close(x) {
+        setTimeout(function(){
+            console.log(x, " ");
+        }, x*1000);
+    }
+    close(i);
+    }
+}
+
+x();
+
+
